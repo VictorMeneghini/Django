c4bd1f4a056479f5dd894d20f3fcb874e08c1d1f
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-14 17:41:05 -0300

    Update polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOGIN_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('', include('authentication.urls')),
     path('admin/', admin.site.urls),
 ]
