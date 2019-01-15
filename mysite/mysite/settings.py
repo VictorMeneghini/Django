a4133da302aafd7b42dc9687e7027c81c929662e
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-15 14:54:13 -0300

    Import new app url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
     'polls.apps.PollsConfig',
     'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
