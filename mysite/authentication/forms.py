5186bea6dfaf8e7e7b5ca4cfe21c3fc463a8dcbc
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-16 23:03:37 -0300

    Create form model and refactoring views/template

---
+++ mysite/authentication/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class AuthUser(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}), max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+class SigninUser(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}), max_length=100)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    file = forms.FileField()
