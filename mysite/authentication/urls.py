7638446abbf5269dd50e6b4263e7b348ddfebe98
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-16 23:00:22 -0300

    Refactoring urls paths

--- mysite/authentication/urls.py
+++ mysite/authentication/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 app_name = 'authentication'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login_user/', views.login_user, name='login_user'),
-    path('welcome/', views.welcome, name='welcome'),
+    path('', views.login_user, name='login_user'),
     path('signin_user/', views.signin_user, name="signin_user"),
-    path('logout_user/', views.logout_user, name="logout_user")
+    path('logout_user/', views.logout_user, name="logout_user"),
 ]
